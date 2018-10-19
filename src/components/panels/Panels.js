d07b3afb849fc4a832506f6dddf5d73c2073617a
Ben Wallman <benwallman@hotmail.com>
2018-10-19 13:44:18 +0100

    Wired up adding a cake to the basket with redux :)

--- src/components/panels/Panels.js
+++ src/components/panels/Panels.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -8,7 +10,12 @@ import IconButton from '@material-ui/core/IconButton';
 import red from '@material-ui/core/colors/red';
 import AddIcon from '@material-ui/icons/Add';
 
+import addCake from '../../actions/addCake';
 import cakes from '../../cakes.json';
+
+const mapDispatchToProps = dispatch => ({
+  addCake: () => dispatch(addCake())
+});
 
 const styles = theme => ({
   card: {
@@ -31,14 +38,14 @@ const styles = theme => ({
   }
 });
 
-export const Panels = ({ classes }) => (
+export const Panels = ({ classes, addCake }) => (
   <div className={classes.container}>
     {cakes.map(({ name, imageUrl, cost }) => (
       <Card className={classes.card} key={name}>
         <CardHeader
           action={
             <IconButton>
-              <AddIcon />
+              <AddIcon onClick={addCake} />
             </IconButton>
           }
           title={name}
@@ -56,6 +63,7 @@ export const Panels = ({ classes }) => (
 
 Panels.propTypes = {
   classes: PropTypes.object.isRequired,
+  addCake: PropTypes.func.isRequired,
 };
 
-export default withStyles(styles)(Panels);
+export default connect(null, mapDispatchToProps)(withStyles(styles)(Panels));
--- src/components/panels/Panels.test.js
+++ src/components/panels/Panels.test.js
@@ -3,24 +3,36 @@ import { mount } from 'enzyme';
 
 import { Panels } from './Panels';
 
+const emptyFunction = () => '';
+
 test('displays the names for the all cakes', () => {
-  const wrapper = mount(<Panels classes={{}} />);
+  const wrapper = mount(
+    <Panels classes={{}} addCake={emptyFunction} />
+  );
+
   expect(wrapper.text()).toContain('Beautiful birthday cake');
   expect(wrapper.text()).toContain('Lovely wedding cake');
   expect(wrapper.text()).toContain('Cheesy, clicéd  cake');  
 });
 
 test('displays the prices for all the cakes', () => {
-  const wrapper = mount(<Panels classes={{}} />);
+  const wrapper = mount(
+    <Panels classes={{}} addCake={emptyFunction} />
+  );
+
   expect(wrapper.text()).toContain('£17.24');
   expect(wrapper.text()).toContain('£33.02');
   expect(wrapper.text()).toContain('£9.99');  
 });
 
 test('displays the images for all the cakes', () => {
-  const wrapper = mount(<Panels classes={{}} />);
+  const wrapper = mount(
+    <Panels classes={{}} addCake={emptyFunction} />
+  );
+
   const cardImages = wrapper.find('CardMedia');
   expect(cardImages).toHaveLength(3);
+
   const expectedImages = [
     "https://images.pexels.com/photos/433527/pexels-photo-433527.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "https://images.pexels.com/photos/2226/food-couple-sweet-married.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
@@ -29,4 +41,13 @@ test('displays the images for all the cakes', () => {
   cardImages.forEach((Node, index) => {
     expect(Node.prop('image')).toBe(expectedImages[index]);
   });
+});
+
+test('clicking Add Icon should increase the number of cakes in the basket', () => {
+  const spy = jest.fn();
+  const wrapper = mount(<Panels classes={{}} addCake={spy} />);
+
+  const firstAddIcon = wrapper.find('AddIcon').first();
+  firstAddIcon.simulate('click');
+  expect(spy).toHaveBeenCalled();
 });
